51f945cb051b0f6cbacc341f5f8e8c7503e6516c
Renso Guilalas <rensoglorenzo@gmail.com>
2023-02-15 23:20:25 +0000

    Project Work.

--- Project_1/PizzaShop/PizzaOrder/models.py
+++ Project_1/PizzaShop/PizzaOrder/models.py
@@ -64,11 +64,15 @@ class PizzaOrder(models.Model):
 
 class DeliveryDetail(models.Model):
     id = models.AutoField(primary_key=True)
-    first_name = models.TextField()
-    last_name = models.TextField()
-    address = models.TextField()
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    address1 = models.CharField(max_length=50, default='')
+    address2 = models.CharField(max_length=50, default='')
+    county = models.CharField(max_length=50, default='')
+    eircode = models.CharField(max_length=7, default='')
     card = models.BigIntegerField()
     expiry = models.DateField(default=date.today)
+    pizza_order = models.ForeignKey(PizzaOrder, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return self.first_name + " " + self.last_name + ", " + self.address + ", " + str(self.card) + ", " + str(self.expiry)
+        return self.first_name + " " + self.last_name + ", " + self.address1 + ", " + self.address2 + ", " + str(self.card) + ", " + str(self.expiry)
--- Project_1/PizzaShop/PizzaOrder/views.py
+++ Project_1/PizzaShop/PizzaOrder/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def view_homepage(request):
+    return render(request, 'homepage.html')
