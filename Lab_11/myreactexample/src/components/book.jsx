8f4f412043a034b27691c718ea5f4a9280f1edfe
Renso Guilalas <renso.guilalas2@mail.dcu.ie>
2023-03-29 23:55:58 +0100

    Completed Lab_11.

--- Lab_11/myreactexample/src/components/book.jsx
+++ Lab_11/myreactexample/src/components/book.jsx
@@ -1,21 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Book({id}) {
-    const [book, setBook] = useState([]);
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [genre, setGenre] = useState("");
+    const [releaseYear, setReleaseYear] = useState("");
+    const [quantity, setQuantity] = useState("");
+    const [isLoaded, setIsLoaded] = useState(false);
 
-    fetch("http://127.0.0.1:8000/api/book/")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.id)
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/api/book/" + id + "/")
+        .then(response => response.json())
+        .then(data => {
+            setTitle(data.title)
+            setAuthor(data.author)
+            setGenre(data.genre)
+            setReleaseYear(data.release_year)
+            setQuantity(data.quantity)
+            setIsLoaded(true)
+        })
+        .catch(err => console.log(err))
     })
-    .catch(err => console.log(err));
 
-    return (
-        <ul>
-            <li>
-            </li>
-        </ul>
-    )
+    if(isLoaded) {
+        return (
+            <ul>
+                <li><p>Title: {title}</p></li>
+                <li><p>Title: {author}</p></li>
+                <li><p>Title: {genre}</p></li>
+                <li><p>Title: {releaseYear}</p></li>
+                <li><p>Title: {quantity}</p></li>
+            </ul>
+        )
+    }
+    else {
+        return (
+            <img src="loading.gif" alt="loading" />
+        )
+    }
+
 }
 
 export default Book;
