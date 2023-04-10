fcf747d5bad332f09de476ecf10a2d4bfaa8844c
Renso Guilalas <renso.guilalas2@mail.dcu.ie>
2023-04-10 22:16:44 +0100

    renamed the first lab.

--- Project_2/university-system/src/components/AllDegrees.jsx
+++ Project_2/university-system/src/components/AllDegrees.jsx
@@ -1,37 +1,61 @@
+import MainLayout from "../layout/MainLayout";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 
 function AllDegrees() {
     const [degrees, setDegrees] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
+    const [searchText, setSearchText] = useState("");
 
     useEffect(() => {
         fetch("http://127.0.0.1:8000/api/degree/")
         .then(response => response.json())
         .then(data => {
-            setDegrees(data.map(e => e.full_name))
+            setDegrees(data)
             setIsLoaded(true)
         })
         .catch(error => console.log(error))
-    })
+    }, []) // Add an empty dependency array to prevent infinite loops
 
     const displayDegrees = () => {
+        const filteredDegrees = degrees.filter(degree =>
+            degree.full_name.toLowerCase().includes(searchText.toLowerCase()) ||
+            degree.shortcode.toLowerCase().includes(searchText.toLowerCase())
+        )
+
         return (
-            degrees.map(elem =>
-                <li key={elem.id}>{elem}</li>
+            filteredDegrees.map(elem =>
+                <div key={elem.shortcode}>
+                    <h2>{elem.full_name}</h2>
+                    <p>Shortcode: {elem.shortcode}</p>
+                    <Link to={`/degree/${elem.shortcode}`}><button>Show Details</button></Link>
+                    <hr />
+                </div>
             )
         )
     }
 
     if(isLoaded) {
         return (
-            <ul>
-                {displayDegrees()}
-            </ul>
+            <MainLayout>
+                <input
+                type="text"
+                value={searchText}
+                placeholder="Search Degree"
+                onChange={e => setSearchText(e.target.value)}
+                />
+                <br />
+                <ul>
+                    {displayDegrees()}
+                </ul>
+            </MainLayout>
         )
     }
     else {
         return (
-            <img src="loading.gif" alt="loading"></img>
+            <MainLayout>
+                <img src="loading.gif" alt="loading"></img>
+            </MainLayout>
         )
     }
 }
--- Project_2/university-system/src/index.js
+++ Project_2/university-system/src/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {
     createBrowserRouter,
     RouterProvider,
 } from "react-router-dom";
 import App from './App';
-import DegreesPage from './pages/DegreesPage';
-import CohortsPage from './pages/CohortsPage';
+// degrees
+import AllDegrees from './components/AllDegrees';
+import SingleDegree from './components/SingleDegree';
+import CreateDegree from './components/CreateDegree';
+
+// cohorts
+import AllCohorts from './components/AllCohorts';
+import SingleCohort from './components/SingleCohort';
+import CreateCohort from './components/CreateCohort';
+
+// modules
+import AllModules from './components/AllModules';
 
 const router = createBrowserRouter([
     {
@@ -18,11 +29,83 @@ const router = createBrowserRouter([
     },
     {
         path: "degrees",
-        element: <DegreesPage />,
+        element: <AllDegrees />,
+    },
+    {
+        path: "degree/COMSCI",
+        element: <SingleDegree degreeCode="COMSCI" />,
+    },
+    {
+        path: "degree/COMBUS",
+        element: <SingleDegree degreeCode="COMBUS" />,
+    },
+    {
+        path: "degree/DS",
+        element: <SingleDegree degreeCode="DS" />,
+    },
+    {
+        path: "degree/create",
+        element: <CreateDegree />,
     },
     {
         path: "cohorts",
-        element: <CohortsPage />,
+        element: <AllCohorts />,
+    },
+    {
+        path: "cohort/COMBUS1",
+        element: <SingleCohort cohortCode="COMBUS1" />,
+    },
+    {
+        path: "cohort/COMBUS2",
+        element: <SingleCohort cohortCode="COMBUS2" />,
+    },
+    {
+        path: "cohort/COMBUS3",
+        element: <SingleCohort cohortCode="COMBUS3" />,
+    },
+    {
+        path: "cohort/COMBUS4",
+        element: <SingleCohort cohortCode="COMBUS4" />,
+    },
+    {
+        path: "cohort/COMSCI1",
+        element: <SingleCohort cohortCode="COMSCI1" />,
+    },
+    {
+        path: "cohort/COMSCI2",
+        element: <SingleCohort cohortCode="COMSCI2" />,
+    },
+    {
+        path: "cohort/COMSCI3",
+        element: <SingleCohort cohortCode="COMSCI3" />,
+    },
+    {
+        path: "cohort/COMSCI4",
+        element: <SingleCohort cohortCode="COMSCI4" />,
+    },
+    {
+        path: "cohort/DS1",
+        element: <SingleCohort cohortCode="DS1" />,
+    },
+    {
+        path: "cohort/DS2",
+        element: <SingleCohort cohortCode="DS2" />,
+    },
+    {
+        path: "cohort/DS3",
+        element: <SingleCohort cohortCode="DS3" />,
+    },
+    {
+        path: "cohort/DS4",
+        element: <SingleCohort cohortCode="DS4" />,
+    },
+    {
+        path: "cohort/create",
+        element: <CreateCohort />,
+    },
+    {
+        path: "modules",
+        element: <AllModules />,
     },
 ]);
 
